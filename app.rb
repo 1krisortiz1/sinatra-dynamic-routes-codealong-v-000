7c2ae6905f39aee45190a3ac30cc938cbec463c4
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-01-31 02:02:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,8 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    @user_input = params[:num1, :num2]
-    "#{num1} * #{num2}"
+    @product = (@num1 = params[:num1]) * (@num2 = params[:num2])
+    "#{@product}
   end
 
 end
